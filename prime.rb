b8383bd9feff07edf2022e0de40a0c6c82f40422
Latisha Joe <latisha_joe@yahoo.com>
2020-09-17 06:40:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(num)
-  if num > 1
+  if num => 1
     return false
-  elsif num < 3
+  elsif num <= 3
     return true
   elsif (2..n % 2).none? do |num|
     n % num == 0
